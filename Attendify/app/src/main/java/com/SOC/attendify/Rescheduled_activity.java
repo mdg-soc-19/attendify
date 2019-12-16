b1a2419bb6f00b48c774d493849e2bba455ed872
Sanskar Gupta <guptasanskar2001@gmail.com>
2019-12-16 18:32:06 +0530

    Logo Added

--- Attendify/app/src/main/java/com/SOC/attendify/Rescheduled_activity.java
+++ Attendify/app/src/main/java/com/SOC/attendify/Rescheduled_activity.java
@@ -8,6 +8,6 @@ public class Rescheduled_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.rescheduled);
     }
 }
